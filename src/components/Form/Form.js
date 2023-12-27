27e01245edfb637b87ea6879adda4005cee5b087
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-12-27 22:57:17 +0200

    add logic for response handling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,58 @@
-import React, { useState } from "react";
-import {Container, Typography, Box, Stack, TextField, Button} from "@mui/material";
-import { useParams } from "react-router-dom";
-import templatesList from '../../json/templatesList.json';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Typography,
+  Box,
+  Stack,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import templatesList from "../../../public/templatesList.json";
 import "./form.scss";
 import { api } from "../../chatGpt/chatApi";
+import { useDispatch } from "react-redux";
+import { setResult } from "../../redux/sliceResult";
 
 function Form() {
   const { id } = useParams();
-  const templates = templatesList.templates;
-  const currentTemplate = templates[id > 0 ? id -1 : id];
-  const [promt, setPromt] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [promt, setPromt] = useState("");
+
+  const { templates, responseStructure } = templatesList;
+
+  let currentTemplateIndex = +id - 1;
+  const currentTemplate = templates[currentTemplateIndex] ?? {};
+
+  useEffect(() => {
+    if (typeof +id !== "number" || +id < 1 || isNaN(+id)) {
+      navigate("/forms/1");
+    }
+
+    if (+id > templates.length) {
+      navigate(`/forms/${templates.length}`);
+    }
+  }, []);
 
   const sendPromt = async () => {
-   try {
-     const fullPromt = currentTemplate.email.replace("%Email%", promt);
-     const res = await api.sendMessage(fullPromt);
-     console.log(res, 'res');
-   } catch (e) {
-     console.log(e, 'Error');
-   }
+    try {
+      const populatedInstructions = currentTemplate?.instructions?.replace(
+        "%Email%",
+        promt
+      );
+      const fullPrompt = `${populatedInstructions} \n ${currentTemplate.criteria} \n ${responseStructure}`;
+
+      const res = await api.sendMessage(fullPrompt);
+
+      const parsedResult = JSON.parse(res?.text);
+      if (parsedResult?.table) {
+        dispatch(setResult(parsedResult.table));
+        navigate("/result");
+      }
+    } catch (e) {
+      console.log(e, "Error");
+    }
   };
 
   return (
@@ -30,36 +64,37 @@ function Form() {
       <Typography variant="h5" p={"24px"}>
         Insert your promt
       </Typography>
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-        pb={"24px"}
-      >
+      <Box component="form" noValidate autoComplete="off" pb={"24px"}>
         <TextField
-            variant='outlined'
-            label='Promt'
-            placeholder='Leave your promt...'
-            multiline
-            minRows={3}
-            fullWidth
-            value={promt}
-            onChange={(e) => setPromt(e.target.value)}
+          variant="outlined"
+          label="Promt"
+          placeholder="Leave your promt..."
+          multiline
+          minRows={3}
+          fullWidth
+          value={promt}
+          onChange={(e) => setPromt(e.target.value)}
         />
         <Stack
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            mt={2}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          mt={2}
         >
-          <Button disabled={!promt} variant="contained" size="small" onClick={() => {
-            sendPromt();
-          }}>Send</Button>
+          <Button
+            disabled={!promt}
+            variant="contained"
+            size="small"
+            onClick={() => {
+              sendPromt();
+            }}
+          >
+            Send
+          </Button>
         </Stack>
       </Box>
-
     </Container>
-  )
+  );
 }
 
 export default Form;
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -10,10 +10,11 @@ import {
   TableHead,
   TableRow,
   TableBody,
-  Paper, TableCell
+  Paper,
+  TableCell,
 } from "@mui/material";
-import { tableCellClasses } from '@mui/material/TableCell';
-import { styled } from '@mui/material/styles';
+import { tableCellClasses } from "@mui/material/TableCell";
+import { styled } from "@mui/material/styles";
 import "./result.scss";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -27,51 +28,55 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
 function Result() {
-  const rows = useSelector(state => state.result.gptResult);
+  const rows = useSelector((state) => state.result.gptResult);
 
   return (
-      <Container className="result-container" maxWidth>
-        <Typography variant="h6" pt={"24px"}>
-          Your email
-        </Typography>
-        <Typography variant="body1">Insert email here</Typography>
-        <Typography variant="h6" pt={"24px"}>
-          Result
-        </Typography>
-        <Box pb={"24px"}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Col1</StyledTableCell>
-                  <StyledTableCell align="right">Col2</StyledTableCell>
-                  <StyledTableCell align="right">Col3</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                    <StyledTableRow key={row.name}>
-                      <StyledTableCell component="th" scope="row">{row.name}</StyledTableCell>
-                      <StyledTableCell align="right">{row.name2}</StyledTableCell>
-                      <StyledTableCell align="right">{row.name3}</StyledTableCell>
-                    </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-      </Container>
-  )
+    <Container className="result-container" maxWidth>
+      <Typography variant="h6" pt={"24px"}>
+        Your email
+      </Typography>
+      <Typography variant="body1">Insert email here</Typography>
+      <Typography variant="h6" pt={"24px"}>
+        Result
+      </Typography>
+      <Box pb={"24px"}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Criteria</StyledTableCell>
+                <StyledTableCell align="right">Rank</StyledTableCell>
+                <StyledTableCell align="right">Description</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.criteria}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.rank}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    {row.description}
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Container>
+  );
 }
 
 export default Result;
