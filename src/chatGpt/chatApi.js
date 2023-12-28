c73b9f913f2a954b27826a02b2bca9556988fe6b
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-12-28 15:05:57 +0200

    upd key

--- src/chatGpt/chatApi.js
+++ src/chatGpt/chatApi.js
@@ -2,6 +2,6 @@ import { ChatGPTAPI } from "chatgpt";
 
 export const api = new ChatGPTAPI({
   // apiKey: process.env.OPENAI_API_KEY
-  apiKey: "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",
+  apiKey: "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",
   fetch: window.fetch.bind(this),
 });
